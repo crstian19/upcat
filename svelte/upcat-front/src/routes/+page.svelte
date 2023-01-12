c8d8bc89dd83593c3058ef099a3a833eb3eefd1f
Cristian Gutierrez <cristian@crstian.me>
2023-01-12 22:14:29 +0100

    Init skeleton

---
+++ svelte/upcat-front/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import About from "./about/+page.svelte";
+
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button>Click to close</button>
+
+<a href="/about">Go to about</a>
+<button go to="/about">About</button>
